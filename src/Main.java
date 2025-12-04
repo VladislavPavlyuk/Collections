138b2bd3bd1c0fc8458e4cbd35d666a51daca6de
Vlad Pavliuk <vladpavliuk@gmail.com>
2025-12-04 07:22:23 +0100

    Added demonstrateBasicListOperations() to Lists.java

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,20 @@ import collections.Maps;
 import collections.Sets;
 
  // Main class demonstrating Java Collections Framework.
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("========================================");
         System.out.println("Java Collections Framework Demonstration");
         System.out.println("========================================");
-        
+
+
         // Demonstrate Lists using interface
         System.out.println("\n" + "=".repeat(50));
         System.out.println("LISTS DEMONSTRATION");
         System.out.println("=".repeat(50));
         IListDemonstrator listDemonstrator = new Lists();
         listDemonstrator.demonstrateAll();
+
 
         // Demonstrate Sets using interface
         System.out.println("\n" + "=".repeat(50));
@@ -27,13 +28,15 @@ public class Main {
         ISetDemonstrator setDemonstrator = new Sets();
         setDemonstrator.demonstrateAll();
 
+
         // Demonstrate Maps using interface
         System.out.println("\n" + "=".repeat(50));
         System.out.println("MAPS DEMONSTRATION");
         System.out.println("=".repeat(50));
         IMapDemonstrator mapDemonstrator = new Maps();
         mapDemonstrator.demonstrateAll();
-        
+
+
         System.out.println("\n" + "=".repeat(50));
         System.out.println("Demonstration completed successfully!");
         System.out.println("=".repeat(50));
